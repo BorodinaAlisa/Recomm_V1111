9cfb5daf757d055c889f12ca92a1a22371a0a009
Алиса <@LAPTOP-QB2I6692>
2024-04-23 16:30:00 +0300

    Добавьте файлы проекта.

---
+++ Favorite.cs
@@ -0,0 +1,21 @@
+﻿using DB_993;
+using Microsoft.EntityFrameworkCore;
+
+namespace design
+{
+    public partial class Favorite : Form
+    {
+        public Favorite()
+        {
+            InitializeComponent();
+        }
+        private void LoadFavorites()
+        {
+            using (var context = new ApplicationContex())
+            {
+                var favorites = context.Favourites.Include(f => f.Realty_id).ToList();
+                // Здесь вы можете отобразить объекты из favorites в пользовательском интерфейсе
+            }
+        }
+    }
+}
